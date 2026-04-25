1f13ec2ec99c052afaa595943c00af196e60a02f
slipdaly <ghtscgm@aliyun.com>
2026-04-25 13:11:18 +0800

    fix openwrt

--- scripts/generate-versioned-makefiles.sh
+++ scripts/generate-versioned-makefiles.sh
@@ -45,6 +45,41 @@ awk -v version="$RELEASE_VERSION" -v pkg_hash="$PKG_HASH" '
   { print }
 ' openwrt-support/rtp2httpd/Makefile > openwrt-support/rtp2httpd/Makefile.versioned
 
+# Process rtp2httpd-minimal Makefile
+awk -v version="$RELEASE_VERSION" -v pkg_hash="$PKG_HASH" '
+  # Skip comment lines about version extraction and Makefile.versioned
+  /^# Extract version from git tags/ { next }
+  /^# For firmware maintainers:/ { next }
+  /^# version and PKG_HASH/ { next }
+  /^# See https:\/\/rtp2httpd.com/ { next }
+  # Handle multi-line RELEASE_VERSION block
+  /^RELEASE_VERSION:=/ {
+    # Print hardcoded version instead
+    print "RELEASE_VERSION:=" version
+    # Skip continuation lines (ending with \)
+    while (/\\$/) {
+      if ((getline) <= 0) break
+    }
+    next
+  }
+  # Skip Build/Prepare block
+  /^define Build\/Prepare$/ {
+    while (!/^endef$/) {
+      if ((getline) <= 0) break
+    }
+    next
+  }
+  # Add PKG_SOURCE lines before include $(INCLUDE_DIR)/package.mk
+  /^include \$\(INCLUDE_DIR\)\/package\.mk$/ {
+    print "PKG_SOURCE:=rtp2httpd-v$(RELEASE_VERSION).tar.gz"
+    print "PKG_SOURCE_URL:=https://codeload.github.com/stackia/rtp2httpd/tar.gz/v$(RELEASE_VERSION)?"
+    print "PKG_HASH:=" pkg_hash
+    print ""
+  }
+  # Print all other lines
+  { print }
+' openwrt-support/rtp2httpd-minimal/Makefile > openwrt-support/rtp2httpd-minimal/Makefile.versioned
+
 # Process luci-app-rtp2httpd Makefile
 awk -v version="$RELEASE_VERSION" '
   # Skip comment lines about version extraction and Makefile.versioned
@@ -68,6 +103,9 @@ awk -v version="$RELEASE_VERSION" '
 
 echo "=== rtp2httpd/Makefile.versioned ==="
 cat openwrt-support/rtp2httpd/Makefile.versioned
+echo ""
+echo "=== rtp2httpd-minimal/Makefile.versioned ==="
+cat openwrt-support/rtp2httpd-minimal/Makefile.versioned
 echo ""
 echo "=== luci-app-rtp2httpd/Makefile.versioned ==="
 cat openwrt-support/luci-app-rtp2httpd/Makefile.versioned
